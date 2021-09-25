c817742a1c76f22e9092d5843396f98ed00e34a4
Camilo Ortiz <camiloorvi@gmail.com>
2021-09-25 13:52:24 -0500

    Se agrega funcionalidad para que muestre con un alert el usuario y contraseña

--- ventrack/src/pages/login.jsx
+++ ventrack/src/pages/login.jsx
@@ -1,21 +1,25 @@
+import React, { Component, useRef } from 'react'
 
 const Login = ()=> {
+
+  const handleClickEvent = () => {
+     const name = document.getElementById("name").value;
+     const pass = document.getElementById("pass").value;
+     alert("Usuario:" + name + " " + "Contraseña:" + pass)
+  }
   return (  
-    
     <>
       <div className='card'>
      
         <form action="" className='borderAcceso'>
-              <label htmlFor="">Usuario: </label>
-              <input type="text" />
+              <label>Usuario: </label>
+              <input type="text" id="name"/>
               <br />
-              <label htmlFor="">Contraseña: </label>
-              <input type="password" />
+              <label>Contraseña: </label>
+              <input type="password" id="pass"/>
               <br />
-              <input type="submit" value="Ingresar"></input>
+              <input type="submit" value="Ingresar" onClick={handleClickEvent}></input>
             </form> 
-      
-       
       </div>
     </>
     );
