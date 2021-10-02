1ff3caf9e9de3f5961c81fa328041df38be45216
SaraOchoa8a <88735518+SaraOchoa8a@users.noreply.github.com>
2021-10-01 21:27:13 -0500

    Se agregan estilos a la tabla de productos

--- ventrack/src/pages/listaProductos.jsx
+++ ventrack/src/pages/listaProductos.jsx
@@ -3,39 +3,75 @@ import { Link } from 'react-router-dom'
 
 const listaProductos = () => {
     return (
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center table-fixed">
             <Link to ='/registroProducto'>
             <button type="button" className="absolute top-20 right-20 bg-indigo-500 
               text-white rounded border p-2 w-1/6 hover:bg-blue-400">Agregar producto</button>
             </Link>
             <h2 className="text-4xl font-serif my-20">Todos los productos</h2>
-            <table className="bg-gray-300">
+            <table className="border-separate">
                 <thead>
-                <tr>
-                    <th>Identificador único</th>
-                    <th>Descripción</th>
-                    <th>Valor unitario</th>
-                    <th>Estado</th>
+                <tr className="bg-gray-300">
+                    <th className="w-1/6 border border-gray-600 text-gray-800">Identificador único</th>
+                    <th className="w-1/3 border border-gray-600 text-gray-800">Descripción</th>
+                    <th className="w-1/6 border border-gray-600 text-gray-800">Valor unitario</th>
+                    <th className="w-1/6 border border-gray-600 text-gray-800">Estado</th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr>
-                    <td>123</td>
-                    <td>Esta es la descripcion del producto 1</td>
-                    <td>10.000</td>
-                    <td>Disponible</td>
+                    <td className="border border-gray-600">123</td>
+                    <td className="border border-gray-600">Esta es la descripcion del producto 1</td>
+                    <td className="border border-gray-600">10.000</td>
+                    <td className="border border-gray-600">Disponible</td>
+                </tr>
+                <tr className="bg-gray-200">
+                    <td className="border border-gray-600">456</td>
+                    <td className="border border-gray-600">Esta es la descripcion del producto 2</td>
+                    <td className="border border-gray-600">20.000</td>
+                    <td className="border border-gray-600">No disponible</td>
                 </tr>
                 <tr>
-                    <td>456</td>
-                    <td>Esta es la descripcion del producto 1</td>
-                    <td>20.000</td>
-                    <td>No disponible</td>
+                    <td className="border border-gray-600">789</td>
+                    <td className="border border-gray-600">Esta es la descripcion del producto 3</td>
+                    <td className="border border-gray-600">30.000</td>
+                    <td className="border border-gray-600">No disponible</td>
+                </tr>
+                <tr className="bg-gray-200">
+                    <td className="border border-gray-600">987</td>
+                    <td className="border border-gray-600">Esta es la descripcion del producto 4</td>
+                    <td className="border border-gray-600">40.000</td>
+                    <td className="border border-gray-600">Disponible</td>
                 </tr>
                 <tr>
-                    <td>789</td>
-                    <td>Esta es la descripcion del producto 1</td>
-                    <td>30.000</td>
-                    <td>No disponible</td>
+                    <td className="border border-gray-600">654</td>
+                    <td className="border border-gray-600">Esta es la descripcion del producto 5</td>
+                    <td className="border border-gray-600">50.000</td>
+                    <td className="border border-gray-600">Disponible</td>
+                </tr>
+                <tr className="bg-gray-200">
+                    <td className="border border-gray-600">321</td>
+                    <td className="border border-gray-600">Esta es la descripcion del producto 6</td>
+                    <td className="border border-gray-600">60.000</td>
+                    <td className="border border-gray-600">No disponible</td>
+                </tr>
+                <tr>
+                    <td className="border border-gray-600">132</td>
+                    <td className="border border-gray-600">Esta es la descripcion del producto 7</td>
+                    <td className="border border-gray-600">70.000</td>
+                    <td className="border border-gray-600">Disponible</td>
+                </tr>
+                <tr className="bg-gray-200">
+                    <td className="border border-gray-600">354</td>
+                    <td className="border border-gray-600">Esta es la descripcion del producto 8</td>
+                    <td className="border border-gray-600">80.000</td>
+                    <td className="border border-gray-600">No disponible</td>
+                </tr>
+                <tr>
+                    <td className="border border-gray-600">687</td>
+                    <td className="border border-gray-600">Esta es la descripcion del producto 9</td>
+                    <td className="border border-gray-600">90.000</td>
+                    <td className="border border-gray-600">Disponible</td>
                 </tr>
                 </tbody>
             </table>
