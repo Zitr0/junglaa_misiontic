3dbe1ddb1e59bdc0d74f9f15f46d564a4865a2b7
Camilo Ortiz <camiloorvi@gmail.com>
2021-10-14 23:25:16 -0500

    Actualización de dashboard roles

---
+++ ventrack/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+//Routing Pages
+import Dashboard from './Dashboard.js';
+import Login from './Login.js';
+import Register from './Register.js';
+import Roles from './Roles.js';
+//Router Modules Router for routing, Switch for switching components related to routes, Link for routes linking
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+class App extends React.Component {
+  
+render() {
+  return (
+    <Router>
+      
+        <Switch>
+          <Route exact path='/' component={Dashboard} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/roles' component={Roles} />
+        </Switch>
+      
+  </Router>
+  );
+  
+}
+}
+export default App;
