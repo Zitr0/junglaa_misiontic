f3021f453a26bdd94ce9043f8b1dc26f3af40563
Camilo Ortiz <camiloorvi@gmail.com>
2021-10-20 22:52:49 -0500

    Se modifica el config para despliegue en heroku

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.PORT,
-    db: process.env.MONGODB_URL
+    port: process.env.PORT || 3001,
+    db: process.env.MONGODB_URL || 'mongodb+srv://ventrack_user:FsrIHVCICNjO85O3@cluster0.exonc.mongodb.net/ventrack?retryWrites=true&w=majority'
 
 }
