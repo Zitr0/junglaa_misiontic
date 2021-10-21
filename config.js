de58df73a27f20511c6333fbc92c08e264a1a9ad
Camilo Ortiz <camiloorvi@gmail.com>
2021-10-20 20:30:19 -0500

    Se actualiza config con env

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
 module.exports = {
-    port: process.env.PORT || 3001,
-    db: process.env.MONGODB || 'mongodb+srv://ventrack_user:FsrIHVCICNjO85O3@cluster0.exonc.mongodb.net/ventrack?retryWrites=true&w=majority'
+    port: process.env.PORT,
+    db: process.env.MONGODB_URL
 
 }
-/* 
-    'mongodb://localhost:27017/ventrack'
-*/
